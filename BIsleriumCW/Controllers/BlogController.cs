1226fdad98882ab4a502ba56a424af0c230c1a3d
Anish Koju <anishkoju46@gmail.com>
2024-05-09 23:31:58 +0545

    Upvote downvote count function added

--- BIsleriumCW/Controllers/BlogController.cs
+++ BIsleriumCW/Controllers/BlogController.cs
@@ -359,6 +359,8 @@ namespace BIsleriumCW.Controllers
             // Save changes to persist the new comment in the database
             await _dbContext.SaveChangesAsync();
             int upvotes = _dbContext.BlogReactions.Count(r => r.BlogId == blogId && r.Upvote);
+            blog.UpVote = upvotes;
+            await _dbContext.SaveChangesAsync();
             return Ok(upvotes); // Return 200 OK if the comment was added successfully
         }
 
@@ -411,6 +413,8 @@ namespace BIsleriumCW.Controllers
             // Save changes to persist the new comment in the database
             await _dbContext.SaveChangesAsync();
             int downvotes = _dbContext.BlogReactions.Count(r => r.BlogId == blogId && r.Upvote);
+            blog.DownVote = downvotes;
+            await _dbContext.SaveChangesAsync();
             return Ok(downvotes); // Return 200 OK if the comment was added successfully
         }
 
@@ -461,6 +465,56 @@ namespace BIsleriumCW.Controllers
             {
                 return StatusCode(500, "An error occurred while fetching the user's blogs.");
             }
+        }
+
+        [HttpGet("{blogId}/upvoteCount")]
+        public async Task<ActionResult<int>> GetUpvoteCount(int blogId)
+        {
+            var upvoteCount = await _dbContext.Blogs
+                .Where(b => b.BlogID == blogId)
+                .Select(b => b.UpVote)
+                .FirstOrDefaultAsync();
+
+            if (upvoteCount == 0)
+            {
+                return NotFound(); // No upvotes found for the specified blog
+            }
+
+            return Ok(upvoteCount);
+        }
+
+        // Endpoint to retrieve downvote count of a blog
+        [HttpGet("{blogId}/downvoteCount")]
+        public async Task<ActionResult<int>> GetDownvoteCount(int blogId)
+        {
+            var downvoteCount = await _dbContext.Blogs
+                .Where(b => b.BlogID == blogId)
+                .Select(b => b.DownVote)
+                .FirstOrDefaultAsync();
+
+            if (downvoteCount == 0)
+            {
+                return NotFound(); // No downvotes found for the specified blog
+            }
+
+            return Ok(downvoteCount);
+        }
+
+        // Endpoint to retrieve popularity of a blog
+        [HttpGet("{blogId}/popularity")]
+        public async Task<ActionResult<double>> GetPopularity(int blogId)
+        {
+            var popularity = await _dbContext.Blogs
+                .Where(b => b.BlogID == blogId)
+                .Select(b => b.Popularity)
+                .FirstOrDefaultAsync();
+
+            if (popularity == 0)
+            {
+                return NotFound(); // Popularity not calculated for the specified blog
+            }
+
+            return Ok(popularity);
         }
 
     }
